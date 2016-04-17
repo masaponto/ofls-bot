2e1703844093db0024ad05452e8ab21dd9f7c009
masaponto <masaponto@gmail.com>
2016-04-17 12:40:37 +0900

    [ADD] LGTM

--- ofls_bot.py
+++ ofls_bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datetime
 from ofls_shift import OFLS_SHIFT
 from kamebot import Kamebot
 kame = Kamebot(channel='#ofls')
@@ -8,9 +9,17 @@ kame = Kamebot(channel='#ofls')
 @kame.comment
 def main():
     shift = OFLS_SHIFT()
-    print("OFLSBotだよー。シフトを知らせるよ！")
-    print(shift.date_shift(0))
-    #print(oflsbot.week_shift(1))
+
+    date = datetime.date.today().weekday()
+    SUNDAY = 6
+
+    if date == SUNDAY:
+        print("OFLSBotだよー。\n来週からのシフトをお知らせするよ！")
+        print(shift.week_shift())
+    else:
+        print("OFLSBotだよー。\n今日のシフトをお知らせするよ！")
+        print(shift.date_shift())
+
 
 if __name__ == "__main__":
     main()
