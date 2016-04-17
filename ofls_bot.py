d97d20156458ddd941eb43fd9973fdb4b0b6f527
masaponto <masaponto@gmail.com>
2016-04-17 14:43:30 +0900

    [ADD] add your shift of week

--- ofls_bot.py
+++ ofls_bot.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import os
+import sys
 from ofls_shift import OFLS_SHIFT
 from kamebot import Kamebot
 kame = Kamebot(channel='#ofls')
@@ -13,9 +15,19 @@ def main():
     date = datetime.date.today().weekday()
     SUNDAY = 6
 
+    if os.environ.get("OFLSNAME") == '' or os.environ.get("OFLSNAME") == None:
+        print('environment vars OFLSNAME not found')
+        print('please run follow')
+        print('$echo \'export OFLSKEY=<your-name-goes-here>\' >> ~/.zshenv')
+        sys.exit()
+    else:
+        NAME = os.environ.get("OFLSNAME")
+
     if date == SUNDAY:
-        print("OFLSBotだよー。\n来週からのシフトをお知らせするよ！")
-        print(shift.week_shift())
+        print("OFLSBotだよー。\n来週の" + NAME + "君のシフトをお知らせするよ！")
+        print(shift.get_your_week_shift(NAME, week=1))
+        print("\n全体のシフトはこれ！")
+        print(shift.week_shift(1))
     else:
         print("OFLSBotだよー。\n今日のシフトをお知らせするよ！")
         print(shift.date_shift())
